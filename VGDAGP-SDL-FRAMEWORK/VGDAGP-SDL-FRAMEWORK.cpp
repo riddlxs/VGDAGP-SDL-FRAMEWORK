ab1333e4e000c8502e51c9382999bb712ed4a061
Liana <lianahockin@gmail.com>
2024-11-04 21:53:39 -0500

    SDL SETUP

--- VGDAGP-SDL-FRAMEWORK/VGDAGP-SDL-FRAMEWORK.cpp
+++ VGDAGP-SDL-FRAMEWORK/VGDAGP-SDL-FRAMEWORK.cpp
@@ -1,20 +1,23 @@
-// VGDAGP-SDL-FRAMEWORK.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Liana Hockin
+// November 4th 2024
 #include <iostream>
+#include<SDL.h>
 
-int main()
+int main(int argc, char* args[])
 {
-    std::cout << "Hello World!\n";
+    // initialize SDL subsystems
+    if (SDL_InitSubSystem(SDL_INIT_VIDEO) > 0) {
+        // we have failed to initialize
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+
+
+
+    // Terminate SDL Subsystems
+    //Functions are NOT all uppercase, so... SDL_Quit();
+    //Enums ARE all uppercase! so... SDL_INIT_VIDEO is an enum
+    SDL_Quit();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
